c078638857cd5a28977d796c4f6c74f2cbb8f988
Igor Polonskiy-Buslaev <barlogfire@ya.ru>
2019-09-03 15:21:54 +0300

    in work

--- html-comodo/js/script.js
+++ html-comodo/js/script.js
@@ -1,11 +1,6 @@
 jQuery(function($){
 
-	$(".grid").masonry({
 
-		itemSelector: '.grid_item',
-		columnWidth:'.width-1',
-		gutter: 26,
-	});
 	
 });
 $(window).scroll(function(){
@@ -18,3 +13,54 @@ $(window).scroll(function(){
 
   }
 });
+$(function() {
+       $('.live_streaming_btn_2').on('click', function() {
+          $(".live_streaming_btn_2").addClass("btn_active");
+          $(".upcoming_challanges").addClass("block_active");
+          $(".live_streaming_btn_1").removeClass("btn_active");
+          $(".live_streaming_btn_3").removeClass("btn_active");
+          $(".live_streaming_box").removeClass("block_active");
+          $(".famous_games").removeClass("block_active");
+       });
+    });
+$(function() {
+       $('.live_streaming_btn_1').on('click', function() {
+          $(".live_streaming_btn_1").addClass("btn_active");
+          $(".live_streaming_box").addClass("block_active");
+          $(".live_streaming_btn_2").removeClass("btn_active");
+          $(".live_streaming_btn_3").removeClass("btn_active");
+          $(".upcoming_challanges").removeClass("block_active");
+          $(".famous_games").removeClass("block_active");
+       });
+    });
+$(function() {
+       $('.live_streaming_btn_3').on('click', function() {
+          $(".live_streaming_btn_3").addClass("btn_active");
+          $(".famous_games").addClass("block_active")
+          $(".live_streaming_btn_1").removeClass("btn_active");
+          $(".live_streaming_btn_2").removeClass("btn_active");
+          $(".upcoming_challanges").removeClass("block_active");
+          $(".live_streaming_box").removeClass("block_active");
+       });
+    });
+$(function() {
+       $('.order_1').on('click', function() {
+          $(".order_1").addClass("btn_active");
+          $(".order_2").removeClass("btn_active");
+          $(".order_3").removeClass("btn_active");
+        });
+    });
+$(function() {
+       $('.order_2').on('click', function() {
+          $(".order_2").addClass("btn_active");
+          $(".order_1").removeClass("btn_active");
+          $(".order_3").removeClass("btn_active");
+        });
+    })
+$(function() {
+       $('.order_3').on('click', function() {
+          $(".order_3").addClass("btn_active");
+          $(".order_2").removeClass("btn_active");
+          $(".order_1").removeClass("btn_active");
+        });
+    })
