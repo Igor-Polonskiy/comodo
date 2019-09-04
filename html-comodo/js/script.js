61f2f608989e7daa76ae6092a1df8e5724502e26
Igor Polonskiy <barlogfire@ya.ru>
2019-09-04 15:38:07 +0300

    in work

--- html-comodo/js/script.js
+++ html-comodo/js/script.js
@@ -15,52 +15,44 @@ $(window).scroll(function(){
 });
 $(function() {
        $('.live_streaming_btn_2').on('click', function() {
-          $(".live_streaming_btn_2").addClass("btn_active");
-          $(".upcoming_challanges").addClass("block_active");
-          $(".live_streaming_btn_1").removeClass("btn_active");
-          $(".live_streaming_btn_3").removeClass("btn_active");
-          $(".live_streaming_box").removeClass("block_active");
-          $(".famous_games").removeClass("block_active");
-       });
+
+			$(".live_streaming_btn").removeClass("btn_active");
+	        $(".live_streaming_btn_2").addClass("btn_active");
+	        $(".live_streaming_content").removeClass("block_active");
+	        $(".upcoming_challanges").addClass("block_active");
+        });
     });
 $(function() {
        $('.live_streaming_btn_1').on('click', function() {
-          $(".live_streaming_btn_1").addClass("btn_active");
-          $(".live_streaming_box").addClass("block_active");
-          $(".live_streaming_btn_2").removeClass("btn_active");
-          $(".live_streaming_btn_3").removeClass("btn_active");
-          $(".upcoming_challanges").removeClass("block_active");
-          $(".famous_games").removeClass("block_active");
-       });
+	       	$(".live_streaming_btn").removeClass("btn_active");
+	        $(".live_streaming_btn_1").addClass("btn_active");
+	        $(".live_streaming_content").removeClass("block_active")
+         	$(".live_streaming_box").addClass("block_active");
+        });
     });
 $(function() {
        $('.live_streaming_btn_3').on('click', function() {
-          $(".live_streaming_btn_3").addClass("btn_active");
-          $(".famous_games").addClass("block_active")
-          $(".live_streaming_btn_1").removeClass("btn_active");
-          $(".live_streaming_btn_2").removeClass("btn_active");
-          $(".upcoming_challanges").removeClass("block_active");
-          $(".live_streaming_box").removeClass("block_active");
-       });
+       		$(".live_streaming_btn").removeClass("btn_active");
+        	$(".live_streaming_btn_3").addClass("btn_active");
+        	$(".live_streaming_content").removeClass("block_active")
+        	$(".famous_games").addClass("block_active")
+        });
     });
 $(function() {
        $('.order_1').on('click', function() {
-          $(".order_1").addClass("order_active");
-          $(".order_2").removeClass("order_active");
-          $(".order_3").removeClass("order_active");
+	       	$(".order").removeClass("order_active");
+	        $(".order_1").addClass("order_active");
         });
     });
 $(function() {
        $('.order_2').on('click', function() {
-          $(".order_2").addClass("order_active");
-          $(".order_1").removeClass("order_active");
-          $(".order_3").removeClass("order_active");
+			$(".order").removeClass("order_active");
+        	$(".order_2").addClass("order_active");
         });
     })
 $(function() {
        $('.order_3').on('click', function() {
-          $(".order_3").addClass("order_active");
-          $(".order_2").removeClass("order_active");
-          $(".order_1").removeClass("order_active");
+       		$(".order").removeClass("order_active");
+        	$(".order_3").addClass("order_active");
         });
     })
